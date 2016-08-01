6a46117df4f9e24b6538d68831d0d57925da8cdf
Aminul Islam <aislam@fitch.corp>
2016-08-01 14:02:28 -0400

    udpate

--- src/test/java/com/fitchconnect/api/ApIsmokeTestSuite.java
+++ src/test/java/com/fitchconnect/api/ApIsmokeTestSuite.java
@@ -13,7 +13,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -30,11 +29,12 @@ import com.google.common.io.Resources;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
+import com.mongodb.AggregationOutput;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoException;
 
 import groovy.json.internal.Charsets;
 
@@ -52,33 +52,36 @@ public class ApIsmokeTestSuite {
 	String XappClintIDvalue = "3dab0f06-eb00-4bee-8966-268a0ee27ba0";
 	String acceptValue = "application/vnd.api+json";
 	String contentValue = "application/vnd.api+json";
+	String dataBaseServer;
 
 	@BeforeClass
 	public void executionSetup() {
 		env = System.getProperty("env");
 		System.out.println("Test Execution Environment: " + env);
 		if (env == null) {
-			baseURI = "https://api-qa.fitchconnect.com";
-			this.AuthrztionValue = ("Basic MUlLVk1SMjlJS1lIMllPSjFUQkdGQ0tKSDpFN1Y2Z1FJY3RPeG5KbG8rSVBHaGY0K0tTSGc3LzFpOFJsbVo1Tmd6NUpB");
+			baseURI = "https://api-dev.fitchconnect.com";
+			this.AuthrztionValue = ("Basic NTA4Rk44V1BKTUdGVVI5VFpOREFEV0NCSzpvMVY5bkRCMG8yM3djSHp2eVlHNnZZb01GSkJWdG1KZmEwS20vbUczVWVV");
+			dataBaseServer = "mongoweb-x01";
 		} else if (env.equals("dev")) {
 			baseURI = "https://api-dev.fitchconnect.com";
 			this.AuthrztionValue = ("Basic NTA4Rk44V1BKTUdGVVI5VFpOREFEV0NCSzpvMVY5bkRCMG8yM3djSHp2eVlHNnZZb01GSkJWdG1KZmEwS20vbUczVWVV");
-
+			dataBaseServer = "mongoweb-x01";
 		} else if (env.equals("int")) {
 			baseURI = "https://api-int.fitchconnect.com";
-			this.AuthrztionValue = ("Basic MUtQNk1DVVk0WkU1SDFXVlVBWlJUVjNUSjpPM0owV0orUGVhZ3JqMis1bTBTMkdvdnZKRDBrQUd1R3F6Q0M5REIydjRv");
-
+			this.AuthrztionValue = ("Basic MVNCRFI4MzVTQ1lOVU5CSDJSVk1TU0MxOTpHTExaUlR3QUpRdjVTazV1cXRyZWlqZE9SK01yQTZrU2plVmNuZXdlekow");
+			dataBaseServer = "mongoweb-x01";
 		} else if (env.equals("qa")) {
 			baseURI = "https://api-qa.fitchconnect.com";
 			this.AuthrztionValue = ("Basic MUlLVk1SMjlJS1lIMllPSjFUQkdGQ0tKSDpFN1Y2Z1FJY3RPeG5KbG8rSVBHaGY0K0tTSGc3LzFpOFJsbVo1Tmd6NUpB");
+			dataBaseServer = "mongorisk-q01";
 		} else if (env.equals("stage")) {
 			baseURI = "https://api-stage.fitchconnect.com";
 			this.AuthrztionValue = ("Basic NU5COUFRSDVCSTRDUFZTUktJRUpESjQyNTpDYjFxUXQycHd4VGNKZTg1SjkyRVJmL1JMU1haRUlZSjU3NWR5R3RacDVV");
-
+			dataBaseServer = "mongorisk-int01";
 		} else if (env.equals("prod")) {
 			baseURI = "https://api.fitchconnect.com";
 			this.AuthrztionValue = ("Basic M1FEREJQODMyQ1NKTlMwM1ZQT0NSQ0VFQjpENk9PUWtJVW5uaXhVZlZmL3loVnJhbHNDU1dzaGd0L1NJOGFTSFZEVTJR");
-
+			dataBaseServer = "mongorisk-p01";
 		}
 
 		System.out.println(baseURI);
@@ -3196,51 +3199,67 @@ public class ApIsmokeTestSuite {
 
 	}
 
-	@Test
-	private void TestVerifyDB() {
+@Test
+public void comparingData_535() throws IOException{
+            
+	          boolean failure =false ;
+            
+              try{
 
-		try {
+                  MongoClient mongoClient = new MongoClient(dataBaseServer,27017);
+                     
+                      DB db = mongoClient.getDB("admin");
+                      boolean auth = db.authenticate("reporter","the_call".toCharArray());
+                      
+                       db = mongoClient.getDB("core-1");
+                       
+                       
+                       DBCollection collection = db.getCollection("market_sector");
+                       
+                       String id = "01010103";
 
-			MongoClient mongoClient = new MongoClient("mongoweb-x01", 27017);
+                       DBObject match = new BasicDBObject("$match", new BasicDBObject("MRKT_SCTR_ID", id));
+                       
+                       DBObject project = new BasicDBObject("$project", new BasicDBObject("MRKT_SCTR_ID", 1).append("MRKT_SCTR_DESC", 1));
 
-			DB db = mongoClient.getDB("admin");
-			boolean auth = db.authenticate("reporter", "the_call".toCharArray());
 
-			db = mongoClient.getDB("financial-1");
-
-			if (auth) {
-				System.out.println("TRUE");
-			} else {
-				System.out.println("FALSE");
-			}
-
-			DBCollection collection = db.getCollection("financial_statement");
-
-			DBObject doc = collection.findOne();
-			System.out.println(doc);
-
-			/*
-			 * BasicDBObject allQuery = new BasicDBObject(); BasicDBObject
-			 * fields = new BasicDBObject(); fields.put( "_id","5336288");
-			 */
-
-			/*
-			 * DBCursor cursor = collection.find(allQuery,fields); int i = 1;
-			 * 
-			 * while (cursor.hasNext()) { System.out.println(
-			 * "Inserted Document: "+i); System.out.println(cursor.next()); i++;
-			 * }
-			 */
-
-			System.out.println("Done");
-
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (MongoException e) {
-			e.printStackTrace();
-		}
-
-	}
+                       AggregationOutput output = collection.aggregate(match,project);
+                                              
+                       String MRKT_SCTR_DESC= null; 
+                       
+                       for (DBObject result : output.results()) {
+                           MRKT_SCTR_DESC = (String) result.get("MRKT_SCTR_DESC");
+                           System.out.println("**" +result);
+                           }
+                       
+                       db = mongoClient.getDB("fcapidb");
+                       collection = db.getCollection("marketSector");
+                       
+                       
+                        match = new BasicDBObject("$match", new BasicDBObject("_id", id));
+                       
+                        project = new BasicDBObject("$project", new BasicDBObject("_id", 1).append("name", 1));
+                        
+                        output = collection.aggregate(match,project);
+                        
+                        String name = null; 
+                        
+                        for (DBObject result : output.results()) {
+                               name = (String) result.get("name");
+                               System.out.println(result);
+                               }
+                     
+                        System.out.println(MRKT_SCTR_DESC);
+                        System.out.println(name);
+                        
+                        if(!MRKT_SCTR_DESC.equals(name)){
+                        	failure =true ; 
+                        }
+                    	Assert.assertFalse(failure);
+              }catch(Exception e){
+                     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+           }
+        }
 
 	@Test
 	public void Update_XREF_ISOcountryCd_1019() throws IOException {
@@ -3413,7 +3432,7 @@ public class ApIsmokeTestSuite {
 		AssertJUnit.assertFalse(output.asString().contains("isMissing"));
 	}
 
-	@Test
+	@Test(enabled=false)
 
 	public void FCA_1013() {
 
@@ -3558,7 +3577,7 @@ public class ApIsmokeTestSuite {
 		String metaUrlfilter = baseURI + metaEndPoint + filters;
 
 		Response res = given().header("Authorization", AuthrztionValue).header("X-App-Client-Id", XappClintIDvalue)
-				.header("accept", acceptValue).header("content", contentValue).when().get(metaUrl).then()
+				.header("accept", acceptValue).header("content", contentValue).when().get(metaUrlfilter).then()
 				.body("data", Matchers.isEmptyOrNullString()).body("included", Matchers.isEmptyOrNullString())
 				.contentType(ContentType.JSON).extract().response();
 
