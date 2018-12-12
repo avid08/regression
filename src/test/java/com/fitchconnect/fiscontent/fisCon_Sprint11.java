7e080ba44ae9447dfe038fe636ecd1fe5d2a7e9a
Islam <aislam@fitchgroup.co>
2018-12-12 13:01:18 -0500

    Test Cases update for obligors

--- src/test/java/com/fitchconnect/fiscontent/fisCon_Sprint11.java
+++ src/test/java/com/fitchconnect/fiscontent/fisCon_Sprint11.java
@@ -20,7 +20,7 @@ public class fisCon_Sprint11 extends Configuration {
 	@Test
 	public void FISC_1048_obligors() {
 
-		String IssueResourceURi = baseURI + "/v1/issues/87130571";
+		String IssueResourceURi = baseURI + "/v1/issues/94155092";
 
 		Response res = given().header("Authorization", AuthrztionValue).header("X-App-Client-Id", XappClintIDvalue)
 				.header("accept", acceptValue).header("content", contentValue).contentType("application/vnd.api+json")
@@ -34,33 +34,24 @@ public class fisCon_Sprint11 extends Configuration {
 		Assert.assertFalse(res.asString().contains("isMissing"));
 		Assert.assertFalse(res.asString().contains("isRestricted"));
 
-		/*
-		 * String obligorslink =
-		 * res.path("data[0].relationships.obligors.links.related");
-		 * System.out.println(obligorslink);
-		 */
+		String obligorslink = res.path("data.relationships.obligors.links.related");
+		System.out.println(obligorslink);
 
-		/*
-		 * Response res1 = given().header("Authorization",
-		 * AuthrztionValue).header("X-App-Client-Id", XappClintIDvalue)
-		 * .header("accept", acceptValue).header("content",
-		 * contentValue).contentType("application/vnd.api+json")
-		 * .when().get(obligorslink).then() .statusCode(200)
-		 * .extract().response();
-		 * 
-		 * 
-		 * Assert.assertFalse(res1.asString().contains("isError"));
-		 * Assert.assertFalse(res1.asString().contains("isMissing"));
-		 * Assert.assertFalse(res1.asString().contains("isRestricted"));
-		 */
+		Response res1 = given().header("Authorization", AuthrztionValue).header("X-App-Client-Id", XappClintIDvalue)
+				.header("accept", acceptValue).header("content", contentValue).contentType("application/vnd.api+json")
+				.when().get(obligorslink).then().statusCode(200).extract().response();
+
+		Assert.assertFalse(res1.asString().contains("isError"));
+		Assert.assertFalse(res1.asString().contains("isMissing"));
+		Assert.assertFalse(res1.asString().contains("isRestricted"));
 
 	}
 
 	@Test
 
-	public void FISC_1052_Issue_Analstattributes() {
+	public void FISC_1052_Issue_Analystattributes() {
 
-		String IssueResourceURi = baseURI + "/v1/issues/87130571";
+		String IssueResourceURi = baseURI + "/v1/issues/94155092";
 
 		Response res = given().header("Authorization", AuthrztionValue).header("X-App-Client-Id", XappClintIDvalue)
 				.header("accept", acceptValue).header("content", contentValue).contentType("application/vnd.api+json")
@@ -106,7 +97,7 @@ public class fisCon_Sprint11 extends Configuration {
 
 	public void FISC_1129_TransactionSecurity() {
 
-		String IssueResourceURi = baseURI + "/v1/issues/87130571";
+		String IssueResourceURi = baseURI + "/v1/issues/94155092";
 
 		Response res = given().header("Authorization", AuthrztionValue).header("X-App-Client-Id", XappClintIDvalue)
 				.header("accept", acceptValue).header("content", contentValue).contentType("application/vnd.api+json")
@@ -120,26 +111,17 @@ public class fisCon_Sprint11 extends Configuration {
 		Assert.assertFalse(res.asString().contains("isMissing"));
 		Assert.assertFalse(res.asString().contains("isRestricted"));
 
-		/*
-		 * String transactionSecurityLink =
-		 * res.path("data[0].relationships.transactionSecurities.links.related")
-		 * ;
-		 * 
-		 * System.out.println(transactionSecurityLink);
-		 */
-		/*
-		 * Response res1 = given().header("Authorization",
-		 * AuthrztionValue).header("X-App-Client-Id", XappClintIDvalue)
-		 * .header("accept", acceptValue).header("content",
-		 * contentValue).contentType("application/vnd.api+json")
-		 * .when().get(transactionSecurityLink).then() .statusCode(200)
-		 * .extract().response();
-		 * 
-		 * 
-		 * Assert.assertFalse(res1.asString().contains("isError"));
-		 * Assert.assertFalse(res1.asString().contains("isMissing"));
-		 * Assert.assertFalse(res1.asString().contains("isRestricted"));
-		 */
+		String transactionSecurityLink = res.path("data.relationships.transactionSecurities.links.related");
+
+		System.out.println(transactionSecurityLink);
+
+		Response res1 = given().header("Authorization", AuthrztionValue).header("X-App-Client-Id", XappClintIDvalue)
+				.header("accept", acceptValue).header("content", contentValue).contentType("application/vnd.api+json")
+				.when().get(transactionSecurityLink).then().statusCode(200).extract().response();
+
+		Assert.assertFalse(res1.asString().contains("isError"));
+		Assert.assertFalse(res1.asString().contains("isMissing"));
+		Assert.assertFalse(res1.asString().contains("isRestricted"));
 
 	}
 
@@ -154,8 +136,8 @@ public class fisCon_Sprint11 extends Configuration {
 				.header("X-App-Client-Id", XappClintIDvalue).contentType("application/vnd.api+json").body(myRequest)
 				.with().when().post(dataPostUrl).then().assertThat().statusCode(200).body(containsString("value"))
 				.body(containsString("timeIntervalDate")).body(containsString("FC_PEER_ISSUER_NAMES"))
-				.body(containsString("China Minsheng Banking Corp., Ltd.")).body(containsString("Bank of Beijing")).extract()
-				.response();
+				.body(containsString("China Minsheng Banking Corp., Ltd.")).body(containsString("Bank of Beijing"))
+				.extract().response();
 
 		Assert.assertFalse(fieldResponse.asString().contains("isError"));
 		Assert.assertFalse(fieldResponse.asString().contains("isMissing"));
@@ -266,7 +248,7 @@ public class fisCon_Sprint11 extends Configuration {
 
 	public void FISC_966_MS_Analyst_DC_attribute() {
 
-		String entitiesURL = baseURI +"/v1/entities/1409010";
+		String entitiesURL = baseURI + "/v1/entities/1409010";
 
 		Response res = given().header("Authorization", AuthrztionValue).header("X-App-Client-Id", XappClintIDvalue)
 				.header("accept", acceptValue).header("content", contentValue).contentType("application/vnd.api+json")
